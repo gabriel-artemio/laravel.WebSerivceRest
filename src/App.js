fc806df70e321001fe05d370a9ab3615e7523955
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-07-08 22:02:26 -0300

    alimentando select com os dados da api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [servicos, setServicos] = useState([]);
+
+  useEffect(() => {
+    axios.get('URL_DA_SUA_API')
+      .then(response => {
+        setServicos(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar os dados:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>SalaoWeb</p>
+      <h1>SalaoWeb</h1>
+      <p>Aplicação para agendamento de horários</p>
+      <form>
+        <label htmlFor="servicos">Escolha o serviço:</label><br/>
+        <select id="servicos" name="servicos">
+          {servicos.map(servico => (
+            <option key={servico.Id} value={servico.Id}>
+              {servico.Name}
+            </option>
+          ))}
+        </select>
+        <br/><br/>
+        <input type="submit" value="Finalizar Agendamento"/>
+      </form>
     </div>
   );
 }
