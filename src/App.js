329f8eb6f20895ac959609f6af2a001242054885
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-07-09 13:31:30 -0300

    recebendo get servicos e fazendo post agendamento

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
   const [servicos, setServicos] = useState([]);
   const [data, setData] = useState('');
   const [dataInvalida, setDataInvalida] = useState(false);
+  const [servicoId, setServicoId] = useState('');
+  const [telefoneCabelereiro, setTelefoneCabelereiro] = useState('');
+  const [observacao, setObservacao] = useState('');
 
   useEffect(() => {
-    axios.get('URL_DA_SUA_API')
+    axios.get('https://localhost:44312/api/Servico')
       .then(response => {
         setServicos(response.data);
       })
@@ -29,43 +32,83 @@ function App() {
     }
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const agendamento = {
+      id: 0,
+      dataHora: data,
+      servicoId: servicoId,
+      telefoneCabelereiro: telefoneCabelereiro
+    };
+
+    axios.post('https://localhost:44312/api/Horarios', agendamento)
+      .then(response => {
+        console.log('Agendamento realizado com sucesso:', response.data);
+        // Adicionar lógica adicional após o sucesso da requisição, se necessário
+      })
+      .catch(error => {
+        console.error('Erro ao realizar agendamento:', error);
+      });
+  };
+
   return (
     <div className="App container">
-      <h1 class="fw-light">SalãoWeb</h1>
-      <p class="lead text-muted">Agende seu horário no salão com rapidez e praticidade.</p>
-      <form>
+      <h1 className="fw-light">SalãoWeb</h1>
+      <h3 className="fw-light">Você está agendando horário em: nm_salao</h3>
+      <p className="lead text-muted">Agende seu horário no salão com rapidez e praticidade.</p>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="servicos">Escolha o serviço:</label>
-          <select className="form-control" id="servicos" name="servicos">
+          <select
+            className="form-control"
+            id="servicos"
+            name="servicos"
+            value={servicoId}
+            onChange={(e) => setServicoId(e.target.value)}
+          >
+            <option value="">Selecione um serviço</option>
             {servicos.map(servico => (
-              <option key={servico.Id} value={servico.Id}>
-                {servico.Name}
+              <option key={servico.id} value={servico.id}>
+                {servico.nome} - R$: {servico.preco}
               </option>
             ))}
           </select>
         </div>
         <div className="form-group">
           <label htmlFor="data">Escolha a data:</label>
-          <input 
-            type="date" 
-            className={`form-control ${dataInvalida ? 'is-invalid' : ''}`} 
-            id="data" 
-            name="data" 
-            value={data} 
-            onChange={handleDataChange} 
+          <input
+            type="date"
+            className={`form-control ${dataInvalida ? 'is-invalid' : ''}`}
+            id="data"
+            name="data"
+            value={data}
+            onChange={handleDataChange}
           />
           {dataInvalida && <div className="invalid-feedback">A data não pode ser anterior a hoje.</div>}
         </div>
         <div className="form-group">
-          <label htmlFor="horarios">Escolha o horário:</label>
-          <select className="form-control" id="horarios" name="horarios">
-            {servicos.map(servico => (
-              <option key={servico.Id} value={servico.Id}>
-                {servico.Name}
-              </option>
-            ))}
-          </select>
-        </div><br/>
+          <label htmlFor="telefoneCabelereiro">Telefone do Cabelereiro:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="telefoneCabelereiro"
+            name="telefoneCabelereiro"
+            value={telefoneCabelereiro}
+            onChange={(e) => setTelefoneCabelereiro(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="observacao">Observação:</label>
+          <textarea
+            className="form-control"
+            id="observacao"
+            name="observacao"
+            value={observacao}
+            onChange={(e) => setObservacao(e.target.value)}
+          />
+        </div>
+        <br/>
         <button type="submit" className="btn btn-primary">Finalizar Agendamento</button>
       </form>
     </div>
